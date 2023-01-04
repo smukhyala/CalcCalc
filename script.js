1435aa09bc9d2da3029dba258a118a2eb1ab0ea7
smukhyala <smukhyala@gmail.com>
2023-01-04 09:54:49 -0800

    indefinite integral parsing

--- script.js
+++ script.js
@@ -37,8 +37,57 @@ function indefiniteIntegral(expression) {
         const argument = part.slice(3).trim();
         integralParts.push(`log(sin(${argument}))`);
       } else {
-        // Assume
-        const argyement = part.slice(3).trim();
+        const arguement = part.slice(3).trim();
       }
+    }
+}
+
+function derivative(func) {
+    // Step 1: Parse the function string to identify the parts of the function
+    const parts = func.split("+").map(part => part.trim());
+    const derivativeParts = [];
+    for (const part of parts) {
+        if (part.startsWith("sin")) {
+            // Derivative rule for sin: d/dx(sin(x)) = cos(x)
+            const argument = part.slice(3).trim();
+            derivativeParts.push(`cos(${argument})`);
+          } else if (part.startsWith("cos")) {
+            // Derivative rule for cos: d/dx(cos(x)) = -sin(x)
+            const argument = part.slice(3).trim();
+            derivativeParts.push(`-sin(${argument})`);
+          } else if (part.startsWith("tan")) {
+            // Derivative rule for tan: d/dx(tan(x)) = sec^2(x)
+            const argument = part.slice(3).trim();
+            derivativeParts.push(`sec(${argument})^2`);
+          } else if (part.startsWith("sec")) {
+            // Derivative rule for sec: d/dx(sec(x)) = sec(x)tan(x)
+            const argument = part.slice(3).trim();
+            derivativeParts.push(`sec(${argument})tan(${argument})`);
+          } else if (part.startsWith("csc")) {
+            // Derivative rule for csc: d/dx(csc(x)) = -csc(x)cot(x)
+            const argument = part.slice(3).trim();
+            derivativeParts.push(`-csc(${argument})cot(${argument})`);
+          } else if (part.startsWith("cot")) {
+            // Derivative rule for cot: d/dx(cot(x)) = -csc^2(x)
+            const argument = part.slice(3).trim();
+            derivativeParts.push(`-csc(${argument})^2`);
+          } else {
+            // Assume the part is a polynomial and use the power rule
+            const terms = part.split("*").map(term => term.trim());
+            const derivativeTerms = [];
+            for (const term of terms) {
+              const match = term.match(/^([-]?\d*\.?\d*)x?(\^(\d+))?$/);
+              if (match) {
+                const coefficient = parseFloat(match[1]);
+                let exponent = parseInt(match[3]);
+                if (!exponent) {
+                  exponent = 1;
+                }
+                if (exponent > 0) {
+                  derivativeTer
+                }
+              }
+            }
+        }
     }
 }
